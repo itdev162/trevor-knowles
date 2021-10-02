a1412396507805abd4da25085c5ff4a1d09f8aa4
knowlet1 <knowlet1@gmatc.matc.edu>
2021-10-01 21:10:12 -0500

    Update App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        BlogBox
+      </header>
+    </div>
+  );
+}
+}
+export default App;
