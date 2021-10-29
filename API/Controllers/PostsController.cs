67287b8239cb052bd7973a55750f228c819025c0
knowlet1 <knowlet1@gmatc.matc.edu>
2021-10-29 14:05:40 -0500

    add edit changes

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -70,6 +70,36 @@ namespace API.Controllers
             throw new Exception("Error creating post");
         }
 
+        /// <summary>
+        /// PUT api/put
+        /// </summary>
+        /// <param name="request">JSON request containing one or more updated post fields</param>
+        /// <returns>An updated post</returns>
+        [HttpPut]
+        public ActionResult<Post> Update([FromBody] Post request)
+        {
+            var post = context.Posts.Find(request.Id);
+
+            if (request == null)
+            {
+                throw new Exception("Could not find post");
+            }
+
+            // Update the post properties with request values, if present.
+            post.Title = request.Title != null ? request.Title : post.Title;
+            post.Body = request.Body != null ? request.Body : post.Body;
+            post.Date = request.Date != null ? request.Date : post.Date;
+
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return post;
+            }
+
+            throw new Exception("Error updating post");
+        }
+
         
     }
 }
