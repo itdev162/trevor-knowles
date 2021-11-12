f7fcca55fb06a2a9061a67d45d6b47b13983e20b
knowlet1 <knowlet1@gmatc.matc.edu>
2021-11-11 19:05:35 -0600

    Completed Activity 8

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,57 @@
 import React from 'react';
-import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PostList from './components/PostList/PostList';
+import Post from './components/Post/Post';
+import './App.css';
 
 class App extends React.Component {
   state = {
-    values: [] 
+    posts: [],
+    post: null
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
-    .then((Response) => {
-      this.setState( {
-        values: Response.data
+    axios.get('http://localhost:5000/api/posts')
+    .then((response) => {
+      this.setState({
+        posts: response.data
       })
     })
-    .catch((error) => {
-      console.error('Error fetching data: ${error}');
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
     })
   }
 
+  viewPost = (post) => {
+    console.log(`view ${post.title}`);
+    this.setState({
+      post: post
+    });
+  }
+
   render() {
-    return(
-      <div className="App">
-      <header className="App-header">
-        BlogBox
-      </header>
-      {this.state.values.map((value: any) => <div key={value}>{value}</div>)}
-    </div>
-  );
+    const { posts, post } = this.state;
+
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            BlogBox
+          </header>
+          <main className="App-content">
+            <Switch>
+              <Route exact path="/">
+                <PostList posts={posts} clickPost={this.viewPost} />
+              </Route>
+              <Route path="/posts/:postId">
+                <Post post={post} />
+              </Route>
+            </Switch>
+          </main>
+        </div>
+      </Router>
+    );
   }
 }
 
